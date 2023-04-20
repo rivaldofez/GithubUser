5d38525bdc0f7f878a41afdbd91597885045cc53
rivaldofez <rivaldofez@gmail.com>
2023-04-20 15:08:37 +0700

    init project

---
+++ GithubUser/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GithubUser
+//
+//  Created by Rivaldo Fernandes on 20/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
