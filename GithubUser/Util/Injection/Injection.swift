c85a9d1cccb8bb662b725f28b8e6b04d2dca911f
rivaldofez <rivaldofez@gmail.com>
2023-04-21 08:05:50 +0700

    add dependency injection

---
+++ GithubUser/Util/Injection/Injection.swift
@@ -0,0 +1,20 @@
+//
+//  Injection.swift
+//  GithubUser
+//
+//  Created by Rivaldo Fernandes on 21/04/23.
+//
+
+import Foundation
+import RealmSwift
+
+final class Injection: NSObject {
+    private func provideRepository() -> UserRepositoryProtocol {
+      let realm = try? Realm()
+
+      let locale: LocaleDataSource = LocaleDataSource.sharedInstance(realm)
+      let remote: RemoteDataSource = RemoteDataSource.sharedInstance
+
+      return UserRepository.sharedInstance(locale, remote)
+    }
+}
